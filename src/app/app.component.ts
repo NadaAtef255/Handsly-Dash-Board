24fa287e2189bc3b9fdda2cff94911d4f5747b9f
abosaifzzz <mohamedabosaif21@gmail.com>
2024-09-22 15:28:21 +0300

    routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LayoutComponent } from './layout/layout.component';
+import { EngineerListComponent } from './Pages/engineer-list/engineer-list.component';
 
 export const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  {
+    path: '',
+    component: LayoutComponent,
+    title: 'layout',
+    children: [{ path: 'engineer', component: EngineerListComponent }],
+  },
+  // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   // { path: 'engineer/:id', component: EngineerDetailsComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
